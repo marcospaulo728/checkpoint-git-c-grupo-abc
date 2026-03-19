aeb17ebdd3d02bd2c2e612431d198a272043df71
gabrielaangel <gabrielaangel2003@hotmail.com>
2026-03-18 22:13:11 -0300

    adiciona nome da Gabriela

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main(void) {
     printf("Marcos Paulo Sepulveda Sampaio");
     printf("Izabelly de Oliveira Menezes");
-    printf("Otávio Henrique Mancilia Santos);
+    printf("Otávio Henrique Mancilia Santos");
+    printf("Gabriela Angel Araujo de Rezende Silva");
 }
