929219b0b0086abefc5cefd21b884fc5d31fbaa5
izabellyomenezes <izabelly.omenezes@gmail.com>
2026-03-18 22:25:20 -0300

    Quebra de linha

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Marcos Paulo Sepulveda Sampaio");
-    printf("Izabelly de Oliveira Menezes");
-    printf("Otávio Henrique Mancilia Santos");
+    printf("Marcos Paulo Sepulveda Sampaio\n");
+    printf("Izabelly de Oliveira Menezes\n");
+    printf("Otávio Henrique Mancilia Santos\n");
     printf("Gabriela Angel Araujo de Rezende Silva");
 }
